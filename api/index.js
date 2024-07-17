c51d15390e7323b86eb34df97e578d3486924f84
Marc Tofan <tofmana@gmail.com>
2024-07-16 21:47:25 -0700

    api folder created and vercel.json

--- api/index.js
+++ api/index.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 
-const stripe = require("stripe")(STRIPE_PRIVATE_KEY);
-
-const { createSessionController } = require("./controllers/create-session-controller.js");
-const { getAllProductsController } = require("./controllers/get-all-products-controller.js");
+const { createSessionController } = require("../controllers/create-session-controller.js");
+const { getAllProductsController } = require("../controllers/get-all-products-controller.js");
 require("dotenv").config();
 
 const app = express();
@@ -20,3 +18,5 @@ app.get("/get-all-products", getAllProductsController);
 app.post("/create-checkout-session", createSessionController);
 
 app.listen(process.env.PORT || 3001);
+
+module.exports = app;
