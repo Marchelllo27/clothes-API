8d445f66b681e8a515a12103592b61a2220d6cc9
Marc Tofan <tofmana@gmail.com>
2026-09-21 18:19:04 -0700

    test clg added in get-all-products-controller.js

--- controllers/get-all-products-controller.js
+++ controllers/get-all-products-controller.js
@@ -3,7 +3,7 @@ const { fetchAllProducts } = require("./lib/helpers");
 async function getAllProductsController(req, res) {
   try {
     const allProducts = await fetchAllProducts();
-
+    console.log("products fetched");
     res.json({ products: allProducts });
   } catch (error) {
     res.status(500).json({ error: error.message || "Something went wrong" });
